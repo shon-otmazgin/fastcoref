9b2244885cbcc0f778ee4b417037fb09d7ddf493
Shon Otmazgin <shon711@gmail.com>
2022-08-13 19:49:28 +0300

    refactor files

--- run_inference.sh
+++
@@ -1,7 +0,0 @@
-python run.py \
-      --output_file=lingmess_predictions.jsonlines \
-      --model_name_or_path=lingmess-longformer-final-model-dgx02/model \
-      --test_file=prepare_ontonotes/dev.english.jsonlines \
-      --eval_split=test \
-      --max_tokens_in_batch=5000 \
-      --device=cuda:0
