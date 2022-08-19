d00ba2b0929bac1be1b23657e7ba2032a48b877b
Shon Otmazgin <shon711@gmail.com>
2022-08-19 20:12:37 +0300

    fixing en_core_web_sm issues

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastcoref',
-    version='1.1',
+    version='1.2',
     license='MIT',
     author="Shon Otmazgin, Arie Cattan, Yoav Goldberg",
     author_email='shon711@gmail.com',
@@ -15,13 +15,11 @@ setup(
         'scipy>=1.7.3',
         'pandas>=1.3.5',
         'spacy>=3.0.6',
-        'en-core-web-sm',
         'torch>=1.10.0',
         'transformers>=4.11.3',
         'datasets>=2.1.0',
-        'wandb>=0.12.15'
+        'wandb>=0.12.15',
+        'en_core_web_sm@https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.0.0/en_core_web_sm-3.0.0.tar.gz'
       ],
-    dependency_links=[
-        'https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.0.0/en_core_web_sm-3.0.0-py3-none-any.whl',
-       ],
+
 )
