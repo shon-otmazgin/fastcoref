05e1303eb8875c0313a774370ba87b9a24f7c508
Shon Otmazgin <shon711@gmail.com>
2023-05-09 19:57:41 +0300

    version 2.1.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name='fastcoref',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='2.1.4',
+    version='2.1.5',
     license='MIT',
     author="Shon Otmazgin, Arie Cattan, Yoav Goldberg",
     author_email='shon711@gmail.com',
